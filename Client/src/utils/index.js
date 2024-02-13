fbc9bf7fb2b4b168b5a20eba168d81c2291bbfd8
Shreyash Waghmare <118048447+shreyash2003w@users.noreply.github.com>
2024-02-13 19:36:33 +0530

    Update index.js

--- Client/src/utils/index.js
+++ Client/src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL= "https://skillio-backend.onrender.com/api-v1";
+const API_URL= "https://skillio-backend.vercel.app/api-v1";
 
 export const API = axios.create({
     baseURL:API_URL,
